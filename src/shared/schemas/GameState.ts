d9cd83fb004fb8d1e05e3beb72748a969679dd87
Daymond Krueger <34870296+DaymondKrueger@users.noreply.github.com>
2026-03-10 14:34:49 -0600

    Image res setup & structure changes

--- src/shared/schemas/GameState.ts
+++ src/shared/schemas/GameState.ts
@@ -1,27 +1,27 @@
 import { Schema, MapSchema, defineTypes } from "@colyseus/schema";
 
 export class Player extends Schema {
-  x: number = 0;
-  y: number = 0;
-  speed: number = 5;
-  name: string = "";
-  score: number = 0;
+	x: number = 0;
+	y: number = 0;
+	speed: number = 5;
+	name: string = "";
+	score: number = 0;
 }
 
 defineTypes(Player, {
-  x: "number",
-  y: "number",
-  speed: "number",
-  name: "string",
-  score: "number",
+	x: "number",
+	y: "number",
+	speed: "number",
+	name: "string",
+	score: "number",
 });
 
 export class GameState extends Schema {
-  players = new MapSchema<Player>();
-  tickRate: number = 60;
+	players = new MapSchema<Player>();
+	tickRate: number = 60;
 }
 
 defineTypes(GameState, {
-  players: { map: Player },
-  tickRate: "number",
+	players: { map: Player },
+	tickRate: "number",
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,71 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, argv) => {
-  const isDev = argv.mode === "development";
+	const isDev = argv.mode === "development";
 
-  return {
-    entry: "./src/client/index.ts",
-    output: {
-      path: path.resolve(__dirname, "dist/client"),
-      filename: "bundle.[contenthash].js",
-      clean: true,
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js"],
-      alias: {
-        "@shared": path.resolve(__dirname, "src/shared"),
-        "@client": path.resolve(__dirname, "src/client"),
-      },
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [
-            {
-              loader: "ts-loader",
-              options: {
-                configFile: "tsconfig.client.json",
-              },
-            },
-          ],
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-            "css-loader",
-            "sass-loader",
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/client/index.html",
-        filename: "index.html",
-      }),
-      ...(isDev
-        ? []
-        : [new MiniCssExtractPlugin({ filename: "styles.[contenthash].css" })]),
-    ],
-    devtool: isDev ? "inline-source-map" : false,
-  };
+	return {
+		entry: "./src/client/index.ts",
+		output: {
+		path: path.resolve(__dirname, "dist/client"),
+		filename: "bundle.[contenthash].js",
+		clean: true,
+		},
+		resolve: {
+		extensions: [".ts", ".tsx", ".js"],
+		alias: {
+			"@shared": path.resolve(__dirname, "src/shared"),
+			"@client": path.resolve(__dirname, "src/client"),
+		},
+		},
+		module: {
+		rules: [
+			{
+			test: /\.tsx?$/,
+			use: [
+				{
+				loader: "ts-loader",
+				options: {
+					configFile: "tsconfig.client.json",
+				},
+				},
+			],
+			exclude: /node_modules/,
+			},
+			{
+			test: /\.scss$/,
+			use: [
+				isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+				"css-loader",
+				"sass-loader",
+			],
+			},
+			// Handles image imports in TypeScript: import arrowUrl from './res/arrow.png'
+			{
+			test: /\.(png|jpg|jpeg|gif|svg|webp|ico)$/i,
+			type: "asset/resource",
+			generator: {
+				filename: "res/[name][ext]",
+			},
+			},
+		],
+		},
+		plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/client/index.html",
+			filename: "index.html",
+		}),
+		new CopyPlugin({
+			patterns: [
+			{ from: "src/client/res", to: "res" },
+			],
+		}),
+		...(isDev
+			? []
+			: [new MiniCssExtractPlugin({ filename: "styles.[contenthash].css" })]),
+		],
+		devtool: isDev ? "inline-source-map" : false,
+	};
 };
